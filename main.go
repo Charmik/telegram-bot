878a37ef45390c8b9e76b7966d5eb4589e3f249c
alexlovkov <charmik1994@gmail.com>
2018-10-06 22:23:32 +0300

    gc

--- main.go
+++ main.go
@@ -13,13 +13,11 @@ var bot = createBot()
 func handler(w http.ResponseWriter, r *http.Request) {
 	s := r.URL.Path[1:]
 	split := strings.Split(s, "/")
-	if (len(split) < 2) {
-		fmt.Fprintf(w, "not enought parameters, need 2, botname & message")
+	if len(split) < 2 {
+		fmt.Fprintf(w, "not enought parameters, need 2, telegram-user & message")
 		return
 	}
-
 	name := split[0]
-	fmt.Fprintf(w, "Hi there, I love %s | %s", split[0], split[1])
 	if name == "charm" {
 		sendMessageToCharm(split[1])
 	}
@@ -29,41 +27,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
 
-func createBot() (*tgbotapi.BotAPI) {
+func createBot() *tgbotapi.BotAPI {
 	bot, err := tgbotapi.NewBotAPI("605258699:AAG3EeFM-ETkvJ0NivCGs7K8YpNWAwtYqUE")
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	/*
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
-	}
-	*/
 	return bot
 }
 
